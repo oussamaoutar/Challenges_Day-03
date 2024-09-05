7be0a474b5cb536d470e3a6f0666c8861bb83ba9
oussama <outaroussama88@gmail.com>
2024-09-05 11:58:34 +0100

    Ajout Switch au Menu Principal

--- Mini Projet/Mini-Projet-D03.c
+++ Mini Projet/Mini-Projet-D03.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct {
     char* nom;
@@ -6,18 +7,28 @@ typedef struct {
     char* email;
 } Contact;
 
+
 int main() {
     int choix;
     do {
         printf("Saisir un Choix : \n");
         printf("1. Ajouter un Contact\n");
-        printf("2. Supprimer un Contact\n");
-        printf("3. Modifier un Contact\n");
+        printf("2. Modifier un Contact\n");
+        printf("3. Supprimer un Contact\n");
         printf("4. Afficher Tous les Contacts\n");
         printf("5. Rechercher un Contacts\n");
         printf("Tapper un Autre Chiffre pour Quitter.\n");
         printf("====> ");
         scanf("%d", &choix);
+            
+        switch(choix) {
+            case 1: ajouterContact(); break;
+            case 2: modifierContact(); break;
+            case 3: supprimerContact(); break;
+            case 4: afficherContacts(); break;
+            case 5: rechercherContact(); break;
+            default: exit;
+        }
     } while(choix > 0 && choix <= 5);
 
 
