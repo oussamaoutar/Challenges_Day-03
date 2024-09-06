4f085ecdf663aca61a0b5a40ebf27414f787c8d1
oussama <outaroussama88@gmail.com>
2024-09-06 09:23:59 +0100

    Calcul taille chaine sans strlen

--- 01-Strings/Challenge-02.c
+++ 01-Strings/Challenge-02.c
@@ -5,7 +5,12 @@ int main() {
     char chaine[1000];
     printf("Saisir une chaine : ");
     scanf("%[^\n]s", &chaine);
-    printf("La taille de cette chaine est : %d", strlen(chaine));
+    int taille_chaine=0, i=0;
+    char test;
+    while(chaine[taille_chaine]!=NULL) {
+        taille_chaine++;
+    }
+    printf("La taille de cette chaine est : %d", taille_chaine);
 
     return 0;
 }
